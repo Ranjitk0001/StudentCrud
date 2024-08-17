41d1780dfad3ec09501d9182700a0a7ab3463f26
rk <ranjitkale0001@gmail.com>
2024-08-17 19:32:04 +0530

    student component set to routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { StudComponent } from './stud/stud.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"studpath",component:StudComponent}
+];
