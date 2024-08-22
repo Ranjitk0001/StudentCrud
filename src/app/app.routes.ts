99314ffdc2cdfaa89510ebd27e1e3ab61e38fc3d
rk <ranjitkale0001@gmail.com>
2024-08-22 19:46:35 +0530

    Login Form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { StudComponent } from './stud/stud.component';
 import { LoginComponent } from './login/login.component';
+import { AppComponent } from './app.component';
 
 export const routes: Routes = [
+
+    {path:"homepath",component:AppComponent},
     {path:"studpath",component:StudComponent},
     {path:"loginpath",component:LoginComponent},
     
